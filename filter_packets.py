200e0c83b8bc48843bcbcb925457185bf2f590c8
Alyssa <ajkelloff@gmail.com>
2026-04-17 14:29:18 -0400

    part 1 of proj

---
+++ filter_packets.py
@@ -0,0 +1,8 @@
+def filter_packets(input, output):
+    # Read through raw packet data
+    # ONLY captures IMCP recho requests/replys into output file 
+    with open(input, 'r') as infile:
+        open(output, 'w') as outfile:
+            for line in infile:
+                  if "ICMP" in line and ("Echo (ping) request" in line or "Echo (ping) reply" in line):
+                        outfile.write(line)
