5b2aaf5c19cd3f86390301e4ad5408258ec24d0b
Adam Kwolek-Sommerville <141803729+FoolShi@users.noreply.github.com>
2026-04-27 23:01:43 -0400

    finish debug, forgot commit

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,4 +1,4 @@
-def compute_metrics(packets):
+def compute_metrics(packets, node_ip):
     metrics = {
         "requests_sent": 0,
         "requests_recieved": 0,
@@ -15,34 +15,78 @@ def compute_metrics(packets):
     }
 
     # Dictionaries to match request/reply pairs
-    requests = {}
-    replys = {}
+    requests_sent = {}
+    requests_recieved = {}
 
     total_rtt = 0
     rtt_count = 0
+
     total_reply_delay = 0
     reply_delay_count = 0
 
     for pkt in packets:
-        key = (pkt["src_ip"], pkt["des_ip"]), pkt["seq"]
-        if pkt["type"] == "request":
-            metrics["replies_sent"] += 1
-            metrics["total_req_bytes_sent"] += pkt["length"]
-            metrics["total_req_data_sent"] += pkt["payload"]
+        src = pkt["src_ip"] 
+        dst = pkt["des_ip"]
+        seq = pkt["seq"]
 
-            requests[key] = pkt
-        else:
-            metrics["replies_recieved"] += 1
-            reverse_key = (pkt["des_ip"], pkt["src_ip"], pkt["seq"])
-            if reverse_key in requests:
-                request_pkt = requests[reverse_key]
+        #Ignore packets that do not involve this node
+        if src != node_ip and dst != node_ip:
+            continue
+
+        key = (src, dst, seq)
+
+        if pkt["type"] == "request":
+            if src == node_ip:
+                metrics["requests_sent"] += 1
+                metrics["total_req_bytes_sent"] += pkt["length"]
+                metrics["total_req_data_sent"] += pkt["payload"]
+
+                requests_sent[key] = pkt
+
+            elif dst == node_ip:
+                metrics["requests_recieved"] += 1
+                metrics["total_req_bytes_recieved"] += pkt["length"]
+                metrics["total_req_data_recieved"] += pkt["payload"]
+
+                requests_recieved[key] = pkt
+
+        elif pkt["type"] == "reply":
+            reverse_key = (dst, src, seq)
+
+            # RTT
+            if dst == node_ip and reverse_key in requests_sent:
+                metrics["replies_recieved"] += 1
+
+                request_pkt = requests_sent[reverse_key]
+
                 rtt = pkt["time"] - request_pkt["time"]
-                total_rtt += rtt
-                rtt_count += 1
+
+                if rtt > 0:
+                    total_rtt += rtt
+                    rtt_count += 1
+
+            # Reply delay
+            elif src == node_ip:
+                metrics["replies_sent"] += 1
+
+                if reverse_key in requests_recieved:
+                    request_pkt = requests_recieved[reverse_key]
+                    reply_delay = pkt["time"] - request_pkt["time"]
+
+                    if reply_delay > 0:
+                        total_reply_delay += reply_delay
+                        reply_delay_count += 1
     
     if rtt_count > 0:
-        metrics["avg_rtt"] = total_rtt / rtt_count
-        metrics["throughput"] = metrics["total_req_bytes_sent"] / total_rtt
-        metrics["goodput"] = metrics["total_req_data_sent"] / total_rtt
-        
+        # seconds -> milliseconds
+        metrics["avg_rtt"] = (total_rtt / rtt_count) * 1000
+
+        # bytes/sec -> KB/sec
+        metrics["throughput"] = (metrics["total_req_bytes_sent"] / 1000) / total_rtt
+        metrics["goodput"] = (metrics["total_req_data_sent"] / 1000) / total_rtt
+
+    if reply_delay_count > 0:
+        # seconds -> microseconds
+        metrics["avg_reply_delay"] = (total_reply_delay / reply_delay_count) * 1_000_000
+    
     return metrics
