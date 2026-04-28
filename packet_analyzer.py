70556db988dc80135035d5b98573a8fcec735d74
Adam Kwolek-Sommerville <141803729+FoolShi@users.noreply.github.com>
2026-04-27 23:02:09 -0400

    finish debug, forgot commit

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -3,25 +3,30 @@ from packet_parser import parse_packets
 from compute_metrics import compute_metrics
 
 def main():
-    nodes = ["Node1", "Node2", "Node3", "Node4"]
+    nodes = {
+        "Node1": "192.168.100.1", 
+        "Node2": "192.168.100.2",
+        "Node3": "192.168.200.1",
+        "Node4": "192.168.200.2"
+        }
 
-    for node in nodes:
-            input = f"{node}.txt"
-            output = f"{node}_filtered.txt"
+    for node, node_ip in nodes.items():
+            input_file = f"{node}.txt"
+            output_file = f"{node}_filtered.txt"
 
             # Phase 1: filtering
-            filter_packets(input, output)
+            filter_packets(input_file, output_file)
 
             # Phase 2: parsing 
-            packets = parse_packets(output)
+            packets = parse_packets(output_file)
 
             # Phase 3: compute metrics
-            metrics = compute_metrics(packets)
+            metrics = compute_metrics(packets, node_ip)
 
             print(f"Metrics for {node}:")
             for key, value in metrics.items():
                 print(f"{key}: {value}")
-            print("\n")
+            print()
 
 if __name__ == "__main__":
     main()
