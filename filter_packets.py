8ae9f3a133e7da6dc625d10ee2646e12d3cf56db
Alyssa <ajkelloff@gmail.com>
2026-04-27 22:12:35 -0400

    added 'with'

--- filter_packets.py
+++ filter_packets.py
@@ -2,7 +2,7 @@ def filter_packets(input, output):
     # Read through raw packet data
     # ONLY captures IMCP recho requests/replys into output file 
     with open(input, 'r') as infile:
-        open(output, 'w') as outfile:
+        with open(output, 'w') as outfile:
             for line in infile:
-                  if "ICMP" in line and ("Echo (ping) request" in line or "Echo (ping) reply" in line):
-                        outfile.write(line)
+                if "ICMP" in line and ("Echo (ping) request" in line or "Echo (ping) reply" in line):
+                    outfile.write(line)
